db59df67995d690a28a635b6a42bb95b1342dbb4
Kunall Banerjee <hey@kimchiii.space>
2025-01-31 14:41:24 -0500

    fix: parsing issue when multiple options are set
    Fixes #16

--- packages/astro-umami/src/index.ts
+++ packages/astro-umami/src/index.ts
@@ -1,3 +1,127 @@
-import { integration } from "./integration.js";
+import type { AstroIntegration } from "astro";
 
-export default integration;
+type OptionalExceptFor<T, K extends keyof T> = {
+  [P in keyof T]: P extends K ? T[P] : T[P] | undefined;
+} & Pick<T, K>;
+
+interface UmamiOptions {
+  /**
+   * Umami tracks all events and pageviews for you automatically. Override this behavior if you plan on using [tracker functions](https://umami.is/docs/tracker-functions).
+   * @default true
+   */
+  autotrack?: boolean;
+  /**
+   * If you want the tracker to only run on specific domains, add them to this list.
+   *
+   * @example ["mywebsite.com", "mywebsite2.com"]
+   */
+  domains?: string[];
+  /**
+   *
+   * The endpoint where your Umami instance is located.
+   * @default https://cloud.umami.is
+   * @example https://umami-on.fly.dev
+   */
+  endpointUrl?: string;
+  /**
+   * Override the location where your analytics data is sent.
+   */
+  hostUrl?: string;
+  /**
+   * The unique ID of your [website](https://umami.is/docs/add-a-website).
+   */
+  id: string;
+  /**
+   * Assign a custom name to the tracker script.
+   *
+   * @default script.js
+   * @see [https://umami.is/docs/environment-variables](https://umami.is/docs/environment-variables)
+   */
+  trackerScriptName?: string;
+}
+
+async function getInjectableWebAnalyticsContent({
+  mode,
+  options,
+}: {
+  mode: "development" | "production";
+  options: OptionalExceptFor<UmamiOptions, "id">;
+}): Promise<string> {
+  const {
+    autotrack = true,
+    domains = [],
+    endpointUrl = "https://cloud.umami.is",
+    hostUrl = "https://cloud.umami.is",
+    id,
+    trackerScriptName = "script.js",
+  } = options;
+
+  const hostname = new URL(endpointUrl).hostname;
+  const configAsString = [
+    !autotrack ? `script.setAttribute("data-auto-track", "${autotrack}")` : "",
+    domains.length > 0
+      ? `script.setAttribute("data-domains", "${domains.join(",")}")`
+      : "",
+    hostUrl !== "https://cloud.umami.is"
+      ? `script.setAttribute("data-host-url", "${hostUrl}")`
+      : "",
+  ]
+    .filter(Boolean)
+    .join(";\n");
+
+  if (mode === "development") {
+    return `
+      localStorage.setItem("umami.disabled", "1");
+
+      var script = document.createElement("script");
+      var viewTransitionsEnabled = document.querySelector("meta[name='astro-view-transitions-enabled']")?.content;
+
+      script.setAttribute("src", "https://${hostname}/${trackerScriptName}");
+      script.setAttribute("defer", true);
+      script.setAttribute("data-website-id", "${id}");
+      ${configAsString};
+
+      if (!!viewTransitionsEnabled) {
+        script.setAttribute("data-astro-rerun", true);
+      }
+
+      var head = document.querySelector("head");
+      head.appendChild(script);
+    `;
+  }
+
+  return `
+    (function () {
+      var script = document.createElement("script");
+      var viewTransitionsEnabled = document.querySelector("meta[name='astro-view-transitions-enabled']")?.content;
+
+      script.setAttribute("src", "https://${hostname}/${trackerScriptName}");
+      script.setAttribute("defer", true);
+      script.setAttribute("data-website-id", "${id}");
+      ${configAsString};
+
+      if (!!viewTransitionsEnabled) {
+        script.setAttribute("data-astro-rerun", true);
+      }
+
+      var head = document.querySelector("head");
+      head.appendChild(script);
+    })()
+  `;
+}
+
+export default function umamiIntegration(options: OptionalExceptFor<UmamiOptions, "id">): AstroIntegration {
+  return {
+    name: "@yeskunall/astro-umami",
+    hooks: {
+      "astro:config:setup": async ({ command, injectScript }) => {
+        const script = await getInjectableWebAnalyticsContent({
+          mode: command === "dev" ? "development" : "production",
+          options,
+        });
+
+        injectScript("head-inline", script);
+      },
+    },
+  };
+}
--- playground/astro.config.ts
+++ playground/astro.config.ts
@@ -7,9 +7,15 @@ const { default: packageName } = await import("@yeskunall/astro-umami");
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    packageName({ id: "94db1cb1-74f4-4a40-ad6c-962362670409" }),
+    packageName({
+      id: "94db1cb1-74f4-4a40-ad6c-962362670409",
+      domains: ["example.com", "com.example"],
+      hostUrl: "https://analytics.eu.umami.is",
+    }),
     hmrIntegration({
-      directory: createResolver(import.meta.url).resolve("../packages/astro-umami"),
+      directory: createResolver(import.meta.url).resolve(
+        "../packages/astro-umami",
+      ),
     }),
   ],
 });
